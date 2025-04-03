1ec6b7c8fe9a54bd197f6e2533ca827887ed2a26
Alex Horton <ahorton@codeblacktech.org>
2025-04-03 18:55:01 -0500

    Add input to main method

--- CSharp/Palindrome/Palindrome/Program.cs
+++ CSharp/Palindrome/Palindrome/Program.cs
@@ -5,8 +5,10 @@
         static void Main(string[] args)
         {
             //making a change for the palindrome solution
-
+            string input = string.Empty;
             Console.WriteLine("Enter a phrase to evaluate:");
+            input = Console.ReadLine();
+
         }
     }
 }
