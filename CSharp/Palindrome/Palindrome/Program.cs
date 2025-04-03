61285c07ca071f915c9ff546f1ea3916ae1fdacd
Alex Horton <ahorton@codeblacktech.org>
2025-04-03 18:53:20 -0500

    Create new solution for Palindrome check.

---
+++ CSharp/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //making a change for the palindrome solution
+
+            Console.WriteLine("Enter a phrase to evaluate:");
+        }
+    }
+}
